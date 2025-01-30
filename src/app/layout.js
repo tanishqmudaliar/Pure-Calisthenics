b6aa4c31c14a9282a8d4ee3e0943005358a499bb
Tanishq Mudaliar <tanishqmudaliar846@gmail.com>
2025-01-31 02:32:59 +0530

    added all types of icons

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,14 +3,20 @@ import "./globals.css";
 export const metadata = {
   title: "Pure Calisthenics",
   description: "We Do Calisthenics Personal Training. (Online & Offline)",
+  icons: {
+    icon: [
+      { rel: "icon", url: "/favicon.png", type: "image/png", sizes: "32x32" },
+      { rel: "icon", url: "/icon-192.png", sizes: "192x192" },
+      { rel: "icon", url: "/icon-512.png", sizes: "512x512" },
+    ],
+    apple: "/apple-icon.png",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='en'>
-      <body>
-        {children}
-      </body>
+    <html lang="en">
+      <body>{children}</body>
     </html>
   );
 }
