c3d558ceacfd46e4e5d7af9bacb4b30e7bbeb162
Tanishq Mudaliar <tanishqmudaliar846@gmail.com>
2025-03-22 06:09:43 +0530

    minor changes

--- src/pages/SectionSeven.jsx
+++ src/pages/SectionSeven.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import localFont from "next/font/local";
 import AOS from "aos";
 import "aos/dist/aos.css";
